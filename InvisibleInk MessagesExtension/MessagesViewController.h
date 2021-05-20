e530816dbc56076d66fa0f2757ed0982ba1216c7
James Alan Bush <jamesalanbush1972@gmail.com>
2021-05-20 16:16:31 -0400

    Initial Commit

---
+++ InvisibleInk MessagesExtension/MessagesViewController.h
@@ -0,0 +1,12 @@
+//
+//  MessagesViewController.h
+//  InvisibleInk MessagesExtension
+//
+//  Created by Xcode Developer on 5/20/21.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
