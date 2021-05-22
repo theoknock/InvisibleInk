91d4adfe72c660d0fe116edda0c82f8e8f65760c
James Alan Bush <jamesalanbush1972@gmail.com>
2021-05-21 22:52:53 -0400

    Refactored with new name

---
+++ InvisibleInk MessagesExtension/RootMessagesViewController.h
@@ -0,0 +1,17 @@
+//
+//  MessagesViewController.h
+//  InvisibleInk MessagesExtension
+//
+//  Created by Xcode Developer on 5/20/21.
+//
+
+#import <UIKit/UIKit.h>
+#import <Messages/Messages.h>
+#import "CompactMessagesViewController.h"
+#import "ExpandedMessagesViewController.h"
+
+@class ExpandedMessagesViewController, CompactMessagesViewController;
+
+@interface RootMessagesViewController : MSMessagesAppViewController
+
+@end
